172e19a40a2c889673f5f064f8f70b1466dc1bd2
Pxndxs XD <pxndixtyr@gmail.com>
2024-07-08 03:21:12 -0400

    feat: Installed and configured the environment for Shadcn UI 🛠️💻🌐 feat: Installed and tested an accordion component from Shadcn UI 🎛️🔄🧪

--- src/routes/dashboard/pages/accordion.page.tsx
+++ src/routes/dashboard/pages/accordion.page.tsx
@@ -1,8 +1,39 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../../../components/ui/accordion'
+
+const items = [
+  {
+    id: 'item-1',
+    question: 'Is it accessible?',
+    answer: 'Yes. It adheres to the WAI-ARIA design pattern.'
+  },
+  {
+    id: 'item-2',
+    question: 'Can I use it in my project?',
+    answer: 'Yes. It is open source and free to use.'
+  },
+  {
+    id: 'item-3',
+    question: 'Can I customize it?',
+    answer: 'Yes. It is highly customizable.'
+  }
+]
 
 export const AccordionPage = () => {
   return (
     <div>
       <h1> Accordion </h1>
+      {
+        items.map( ( item ) => (
+          <Accordion type="multiple" className="w-full" key={ item.id }>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Is it accessible?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        ) )
+      }
     </div>
   )
 }
